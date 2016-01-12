25410152c0e18a88a006859f21a79eeab26e5a50
Naono Yuichi <naono@showa-sys-eng.co.jp>
2016-01-12 13:13:02 +0900

    SpringBootアプリとしての体裁を整えました。jarパッケージを作成して、起動することも確認しました。

---
+++ novel-shelf-server/src/test/java/me/u6k/novel_shelf/server/AppTest.java
@@ -0,0 +1,15 @@
+
+package me.u6k.novel_shelf.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
